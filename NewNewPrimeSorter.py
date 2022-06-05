0d21cd102935471de1a3addc60b38392aee7b121
Tommo499 <73698910+Tommo499@users.noreply.github.com>
2022-06-05 21:24:51 +0930

    Updates NewNewPrimeSorter.py to make it a function

--- NewNewPrimeSorter.py
+++ NewNewPrimeSorter.py
@@ -1,17 +1,13 @@
 import math
-print(">>>", end=' ')
-limit = int(input('')) + 1
-P = False
-primes = 1
-print("Prime: 2")
-for i in range(3, limit):
-    for f in range(2, math.ceil(math.sqrt(i)) + 1):
-        P = False
-        if i % f == 0:
-            print("Composite: " + str(i))
-            P = True
+
+
+def prime(number):
+    if number == 2: return True
+    if number == 1 or number == 0: return False
+    for f in range(2, math.ceil(math.sqrt(number)) + 1):
+        P = True
+        if number % f == 0:
+            P = False
             break
-    if not P:
-        print("Prime: " + str(i))
-        primes += 1
-print(primes)
+
+    return P
