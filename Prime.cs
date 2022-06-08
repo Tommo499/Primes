85537e8cc56c88c9cf98fb268e9de919012317ec
Tommo499 <73698910+Tommo499@users.noreply.github.com>
2022-06-08 21:58:40 +0930

    Changed Program to function

--- Prime.cs
+++ Prime.cs
@@ -1,39 +1,30 @@
-﻿using System;
+
 namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        static bool Prime(int number)
         {
-            Console.Write(">>> ");
-            string a = Console.ReadLine();
-            int limit = Convert.ToInt32(a);
-            int primes = 1;
-            bool P = false;
-            Console.WriteLine("Prime: 2");
-            for (int i = 3; i < limit; i++)
+            if (number == 2)
             {
-                for (int f = 2; f < Math.Ceiling(Math.Sqrt(i)) + 1; f++)
+                return true;
+            }
+            if (number == 0 || number == 1)
+            {
+                return false;
+            }
+            bool P = false;
+            for (int f = 2; f <= Math.Ceiling(Math.Sqrt(number)); f++)
+            {
+                P = true;
+                if (number % f == 0)
                 {
                     P = false;
-                    if (i % f == 0)
-                    {
-                        P = true;
-                        break;
-                    }
-                }
-                if (P == false)
-                {
-                    Console.WriteLine("Prime: " + i);
-                    primes++;
-                }
-                else
-                {
-                    Console.WriteLine("Composite: " + i);
+                    break;
                 }
             }
-            Console.WriteLine(primes);
-            Console.ReadKey();
+            return P;
         }
-    }
+
+        }
 }
